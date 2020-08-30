d2dd9c8175cd7e0daead1cd50851a572c3b46a56
Balro <47242371+Balro@users.noreply.github.com>
2020-08-30 21:59:46 +0800

    解决dataframe转json时没有写入null的bug。

--- potato-spark/src/main/scala/potato/spark/sql/writer/DataFrameFormatter.scala
+++ potato-spark/src/main/scala/potato/spark/sql/writer/DataFrameFormatter.scala
@@ -1,12 +1,53 @@
 package potato.spark.sql.writer
 
-import org.apache.spark.sql.DataFrame
+import java.io.CharArrayWriter
+
+import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
+import org.apache.spark.sql.catalyst.json.{PotatoJSONOptions, PotatoJacksonGenerator}
+import org.apache.spark.sql.{DataFrame, Encoders, Row}
 
 /**
  * 将ds格式化为给定格式，便于writer输出。
  */
 object DataFrameFormatter {
-  def toJSON(df: DataFrame): DataFrame = df.toJSON.toDF("json")
+  /**
+   * 使用默认df.toJSON方法。
+   */
+  def toJSONRaw(df: DataFrame): DataFrame = df.toJSON.toDF("json")
+
+  /**
+   * 修改过的df.toJSON方法，可以写入null字段。
+   */
+  def toJSON(df: DataFrame): DataFrame = {
+    val rowSchema = df.schema
+    val sessionLocalTimeZone = df.sparkSession.sessionState.conf.sessionLocalTimeZone
+    val field = classOf[DataFrame].getDeclaredField("exprEnc")
+    field.setAccessible(true)
+    df.mapPartitions { iter =>
+      val writer = new CharArrayWriter()
+      // create the Generator without separator inserted between 2 records
+      val gen = new PotatoJacksonGenerator(rowSchema, writer,
+        new PotatoJSONOptions(Map.empty[String, String], sessionLocalTimeZone))
+
+      new Iterator[String] {
+        override def hasNext: Boolean = iter.hasNext
+
+        override def next(): String = {
+          gen.write(field.get(df).asInstanceOf[ExpressionEncoder[Row]].toRow(iter.next()))
+          gen.flush()
+
+          val json = writer.toString
+          if (hasNext) {
+            writer.reset()
+          } else {
+            gen.close()
+          }
+
+          json
+        }
+      }
+    }(Encoders.STRING).toDF("json")
+  }
 
   def toCSV(df: DataFrame, sep: String): DataFrame = df.selectExpr(s"concat_ws('$sep',${df.schema.map(_.name).mkString(",")}) as csv")
 

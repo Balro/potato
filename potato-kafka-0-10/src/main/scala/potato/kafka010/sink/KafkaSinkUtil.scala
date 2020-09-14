8ce49f2353a3dc2c5aa492acba7228b0ea5a62cf
wangruoqi <wangruoqi@owhat.cn>
2020-09-14 11:31:08 +0800

    修复写入kafka时如kafka不可用导致近乎无限等待的问题(每条记录默认均需要等待60s，多条记录则需要等待记录数*60s的时间)。通过参数提前触发失败记录检查，缩短批次作业失败触发时间。

--- potato-kafka-0-10/src/main/scala/potato/kafka010/sink/KafkaSinkUtil.scala
+++ potato-kafka-0-10/src/main/scala/potato/kafka010/sink/KafkaSinkUtil.scala
@@ -1,9 +1,11 @@
 package potato.kafka010.sink
 
 import java.util.Properties
+import java.util.concurrent.atomic.AtomicLong
 
 import org.apache.kafka.clients.producer.{Callback, RecordMetadata}
 import org.apache.spark.TaskContext
+import org.apache.spark.internal.Logging
 import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.dstream.DStream
 import potato.kafka010.conf._
@@ -14,7 +16,7 @@ import potato.kafka010.exception._
  * producer相关参数(取自KafkaProducer_JavaDoc):
  * ["bootstrap.servers","acks","retries","batch.size","linger.ms","buffer.memory","key.serializer","value.serializer"]
  */
-object KafkaSinkUtil {
+object KafkaSinkUtil extends Logging {
   def saveToKafka[K, V](stream: DStream[ProducerRecord[K, V]], props: Properties): Unit = {
     stream.foreachRDD { rdd =>
       saveToKafka(rdd, props)
@@ -23,24 +25,34 @@ object KafkaSinkUtil {
 
   def saveToKafka[K, V](rdd: RDD[ProducerRecord[K, V]], props: Properties): Unit = {
     rdd.foreachPartition { part =>
-      var total = 0L
-      var failed = 0L
+      val total = new AtomicLong(0)
+      val failed = new AtomicLong(0)
+      val record = props.getProperty(POTATO_KAFKA_PRODUCER_FAILED_RECORD_KEY.substring(POTATO_KAFKA_PRODUCER_PREFIX.length), POTATO_KAFKA_PRODUCER_FAILED_RECORD_DEFAULT).toLong
+      val ratio = props.getProperty(POTATO_KAFKA_PRODUCER_FAILED_RATIO_KEY.substring(POTATO_KAFKA_PRODUCER_PREFIX.length), POTATO_KAFKA_PRODUCER_FAILED_RATIO_DEFAULT).toDouble
+      val ratioBackoff = props.getProperty(POTATO_KAFKA_PRODUCER_FAILED_RATIO_BACKOFF_KEY.substring(POTATO_KAFKA_PRODUCER_PREFIX.length), POTATO_KAFKA_PRODUCER_FAILED_RATIO_BACKOFF_DEFAULT).toLong
+      System.out.println(s"---- foreach $total:$failed:$record:$ratio")
       GlobalProducerCache.withProducer[K, V, Unit](props) { producer =>
         part.foreach { f =>
           producer.send(f, new Callback {
             override def onCompletion(metadata: RecordMetadata, exception: Exception): Unit = {
+              total.incrementAndGet()
               if (exception != null) {
                 exception.printStackTrace()
-                failed += 1
+                failed.incrementAndGet()
+                if (failed.get() > record) {
+                  throw SinkFailedException(f"Sink failed in partition[${TaskContext.getPartitionId()}] because failed record $failed over $record")
+                } else if ((total.get() > ratioBackoff || total.get() > record) && failed.get().toDouble / total.get() > ratio) {
+                  throw SinkFailedException(f"Sink failed in partition[${TaskContext.getPartitionId()}] because failed ratio $failed/$total=${failed.get().toDouble / total.get()}%.3f over $ratio%.3f")
+                } else {
+                  logWarning(s"Sink failed record status $failed/$total")
+                }
               }
             }
           })
-          total += 1
         }
         producer.flush()
-        val threshold = props.getProperty("failed.threshold", POTATO_KAFKA_PRODUCER_FAILED_THRESHOLD_DEFAULT).toDouble
-        if (total > 0 && failed.toDouble / total > threshold) {
-          throw SinkFailedException(f"Sink failed record in partition[${TaskContext.getPartitionId()}],failed/total[$failed/$total] reach threshold $threshold%.2f")
+        if (failed.get().toDouble / total.get() > ratio) {
+          throw SinkFailedException(f"Sink failed in partition[${TaskContext.getPartitionId()}] because failed ratio $failed/$total=${failed.get().toDouble / total.get().toDouble}%.3f over $ratio%.3f")
         }
       }
     }
